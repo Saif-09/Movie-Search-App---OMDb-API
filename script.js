b3be48fa67c82e6e4fbeaac65a8099aea6a3571f
Saif-09 <saifmd238@gmail.com>
2023-03-15 06:26:31 +0530

    completed script

--- script.js
+++ script.js
@@ -53,11 +53,11 @@ function displayMovieList(movies) {
 }
 
 //Load Movie Details by Fetching data from API
-function loadMovieDetails(){
+function loadMovieDetails() {
     const searchListMovies = searchList.querySelectorAll('.search-list-item');
     searchListMovies.forEach(movie => {
         // console.log(movie);
-        movie.addEventListener('click', async () =>{
+        movie.addEventListener('click', async () => {
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
             const result = await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=9f0decb8`);
@@ -69,4 +69,30 @@ function loadMovieDetails(){
     });
 }
 
+function displayMovieDetails(details) {
+    resultGrid.innerHTML = `<div class="movie-poster">
+    <img src="${(details.Poster!="N/A")? details.Poster : "no-img" }" alt="movie poster">
+
+    </div>
+<div class="movie-info">
+    <h3 class="movie-title">${details.Title}</h3>
+    <ul class="movie-misc-info">
+        <li class="year">Year: ${details.Year}</li>
+        <li class="ratings">Ratings: ${details.Rated}</li>
+        <li class="released">Released: ${details.Released}</li>
+    </ul>
+    <p class="genre"><strong>Genre: </strong>${details.Genre}</p>
+    <p class="writer"><strong>Writer: </strong>${details.Writer}</p>
+    <p class="actors"><strong>Actors: </strong>${details.Actors}</p>
+    <p class="plot"><strong>Plot: </strong>${details.Plot}</p>
+    <p class="language"><strong>Language: </strong>${details.Language}</p>
+    <p class="awards"><strong><i class = "fas fa-award"></i></strong> ${details.Awards}</p>
+</div>`;
+}
+
+window.addEventListener('click', (event)=>{
+    if(event.target.className != "form-control"){
+        searchList.classList.add('hide-search-list');
+    }
+});
 
